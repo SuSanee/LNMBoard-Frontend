db02aecd9481b712c7cfec5bb9ae42c25fa6237c
SuSanee <anshikaagarwal766@gmail.com>
2025-10-30 02:03:42 +0530

    fake commit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 // src/api.js
 import axios from "axios";
-const API = axios.create({ baseURL: "https://lnmboard-backend.onrender.com/api" });
+const API = axios.create({ baseURL: "https://lnmboard-backend.onrender.com/api" }) ;
 export default API;
